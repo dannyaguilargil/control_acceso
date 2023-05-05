f584f734dd78b2a15a6957c38865a6319f5c32a1
dannyaguilargil <danny.aguilar.gil@gmail.com>
2023-05-05 17:15:10 -0500

    modificando proyecto

--- acceso_up/acceso_up/views.py
+++ acceso_up/acceso_up/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import Template,Context
-from django shortcuts import render
+from django.shortcuts import render
 
-def saludo(request)
-    return HttpResponse("Inicializacion del proyecto up")
+
+    
---
+++ acceso_up/usuarios/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.shortcuts import render
+
+urlpatterns = [
+    path('', views.home, name='home'),
+  
+]
