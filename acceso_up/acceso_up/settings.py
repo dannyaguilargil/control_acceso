71505cdc9e245f3c48e0bdcc3828f6c3da61cc70
dannyaguilargil <danny.aguilar.gil@gmail.com>
2023-05-15 17:41:02 -0500

    agregando vistas 2

--- acceso_up/acceso_up/settings.py
+++ acceso_up/acceso_up/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'acceso_up.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'accesoup',
+       'USER': 'danny',
+       'PASSWORD': 'danny',
+       'HOST': 'localhost',
+       'PORT': '3306',
     }
 }
 
--- acceso_up/usuarios/models.py
+++ acceso_up/usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+    tipodocumento = models.CharField(max_length=40, verbose_name='Tipo de documento'),
+    cedula = models.IntegerField(primary_key=True, verbose_name='Cedula'),
+    nombre = models.CharField(max_length=40, verbose_name='Nombre'),
+    programa = models.CharField(max_length=40, verbose_name='Programa'),
+    email = models.CharField(max_length=40, verbose_name='Email'),
+    telefono = models.CharField(max_length=40, verbose_name='Telefono'),
+    rol = models.CharField(max_length=40, verbose_name='Rol'),
